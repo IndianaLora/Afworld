85d560fcd95c36ba06b4f73c2a5905f687c6edae
Indiana Lora <indiana@ensaludybelleza.com>
2021-03-26 22:53:26 -0400

    Added home page

--- afworld/src/App.js
+++ afworld/src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div id="welcome">
+        <h1>Bienvenido a AfWorld</h1>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src="https://previews.123rf.com/images/vectordivider/vectordivider1609/vectordivider160900259/63397327-a-f-vintage-initials-symbol-letters-a-f-ampersand-surrounded-floral-ornament-wedding-or-business-par.jpg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Hola Amy ☻</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://laresinaepoxi.com/resina-epoxi-manualidades-guia-joyeria/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende mas de las resinas
         </a>
       </header>
     </div>
