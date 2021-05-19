bc15e5b136bd85bf446c40c1e35b309f6673d8be
Indiana Lora <indiana@ensaludybelleza.com>
2021-05-19 12:05:38 -0400

    ...

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,14 +5,14 @@ function Products() {
     <div>
       <h1 className="whiter">Lista de productos</h1>
       <Llavero />
-      <Image/>
+      <Image />
     </div>
   );
 }
 const Llavero = () => {
   return <article className="whiter">Esto es un llavero</article>;
 };
-const Image = () =>{
-   return <img src="../img/Llavero.jpeg" alt="Imagen de nuestro llavero"/>
-}
+const Image = () => {
+  return <img src="../img/Llavero.jpeg" alt="Imagen de nuestro llavero" />;
+};
 export default Products;
